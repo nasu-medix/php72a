39b644958ac06648d430bf04b60a6c0069d3aad8
Takayasu Nasu <nasu@medix-inc.co.jp>
2018-01-29 20:37:59 +0900

    16th commit

--- webhook.php
+++ webhook.php
@@ -7,12 +7,8 @@ if($_SERVER["REQUEST_METHOD"] != "POST") {
 
 $SECRET_KEY = 'test.iprimo';
 $header     = getallheaders();
-$post_data  = file_get_contents('php://input');
 $hmac       = hash_hmac('sha1', $post_data, $SECRET_KEY);
 
 if (isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === 'sha1='.$hmac) {
-  $payload = json_decode($post_data, true);
-  if ($payload['ref'] == 'refs/heads/master') {
-    exec('git pull');
-  }
+  exec('git pull');
 }
