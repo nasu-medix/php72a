518c020d9b71ef9a70761241afa1f66a702867dd
Takayasu Nasu <nasu@medix-inc.co.jp>
2018-01-29 18:48:59 +0900

    second commit

---
+++ webhook.php
@@ -0,0 +1,19 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] != "POST") {
+  header('Location: https:/google.co.jp');
+  exit;
+}
+
+$SECRET_KEY = 'test.iprimo';
+$header     = getallheaders();
+$post_data  = file_get_contents('php://input');
+$hmac       = hash_hmac('sha1', $post_data, $SECRET_KEY);
+
+if (isset($header['X-Hub-Signature'] && $header['X-Hub-Signature'] === 'sha1='.$hmac)) {
+  $payload = json_decode($post_data, true);
+  if ($payload->ref == 'refs/heads/master') {
+    exec('git pull 2>&1');
+  }
+}
+exec('git pull 2>&1');
