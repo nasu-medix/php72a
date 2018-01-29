db08d13e479857595f88016aaff43551e5f67443
Takayasu Nasu <nasu@medix-inc.co.jp>
2018-01-29 19:41:55 +0900

    added hook.log

--- webhook.php
+++ webhook.php
@@ -5,6 +5,7 @@ if($_SERVER["REQUEST_METHOD"] != "POST") {
   exit;
 }
 
+$LOG_FILE   = dirname(__FILE__).'/hook.log';
 $SECRET_KEY = 'test.iprimo';
 $header     = getallheaders();
 $post_data  = file_get_contents('php://input');
@@ -14,7 +15,9 @@ if (isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === 'sha1='.
   $payload = json_decode($post_data, true);
   if ($payload->ref == 'refs/heads/master') {
     exec('git pull');
+    file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['after']." ".$payload['commits'][0]['message']."\n", FILE_APPEND|LOCK_EX);
   }
 } else {
   echo "didn't run git pull.";
+  file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." invalid access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
 }
