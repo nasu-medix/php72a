223150b3fea08aae4b81f14e8e2b08b9654c38e3
Takayasu Nasu <nasu@medix-inc.co.jp>
2018-01-29 20:12:27 +0900

    12th commit

--- webhook.php
+++ webhook.php
@@ -11,18 +11,12 @@ $header     = getallheaders();
 $post_data  = file_get_contents('php://input');
 $hmac       = hash_hmac('sha1', $post_data, $SECRET_KEY);
 
-file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." access start: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
-
 if (isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === 'sha1='.$hmac) {
   $payload = json_decode($post_data, true);
-  file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." payload->ref: ".$payload->ref."\n", FILE_APPEND|LOCK_EX);
-  if ($payload->ref == 'refs/heads/master') {
+  file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." $payload['ref']: ".$payload['ref']."\n", FILE_APPEND|LOCK_EX);
+  if ($payload['ref'] == 'refs/heads/master') {
     exec('git pull');
-    file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['after']." ".$payload['commits'][0]['message']."\n", FILE_APPEND|LOCK_EX);
-  } else {
-    file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." else block: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
   }
 } else {
   echo "didn't run git pull.";
-  file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." invalid access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
 }
