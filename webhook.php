d8ebb62bcbc4b83f3d264c70f4408b194061e191
Takayasu Nasu <nasu@medix-inc.co.jp>
2018-01-29 20:19:45 +0900

    12th commit

--- webhook.php
+++ webhook.php
@@ -16,8 +16,9 @@ if (isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === 'sha1='.
   file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." $payload['ref']: ".$payload['ref']."\n", FILE_APPEND|LOCK_EX);
   if ($payload['ref'] == 'refs/heads/master') {
     exec('git pull');
+  } else {
+    file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." $payload: ".var_dump($payload)."\n", FILE_APPEND|LOCK_EX);
   }
 } else {
-  echo "didn't run git pull.";
   file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." invalid access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
 }
