656a022e149fcd2d4c453321aff58b38addcc068
Takayasu Nasu <nasu@medix-inc.co.jp>
2018-01-29 19:51:06 +0900

    11th commit

--- webhook.php
+++ webhook.php
@@ -11,6 +11,8 @@ $header     = getallheaders();
 $post_data  = file_get_contents('php://input');
 $hmac       = hash_hmac('sha1', $post_data, $SECRET_KEY);
 
+file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." access start: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
+
 if (isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === 'sha1='.$hmac) {
   $payload = json_decode($post_data, true);
   if ($payload->ref == 'refs/heads/master') {
