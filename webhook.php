1a8bf90de6cee6d824768fdfcde89afd38769179
Takayasu Nasu <nasu@medix-inc.co.jp>
2018-01-29 20:25:48 +0900

    12th commit

--- webhook.php
+++ webhook.php
@@ -11,9 +11,10 @@ $header     = getallheaders();
 $post_data  = file_get_contents('php://input');
 $hmac       = hash_hmac('sha1', $post_data, $SECRET_KEY);
 
+file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." start: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
 if (isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === 'sha1='.$hmac) {
   $payload = json_decode($post_data, true);
-  file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." $payload['ref']: ".$payload['ref']."\n", FILE_APPEND|LOCK_EX);
+  file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." $payload: ".var_dump($payload)."\n", FILE_APPEND|LOCK_EX);
   if ($payload['ref'] == 'refs/heads/master') {
     exec('git pull');
   } else {
