1f42ff403277f421da2197edf276b9ffa4125991
Takayasu Nasu <nasu@medix-inc.co.jp>
2018-01-30 13:27:02 +0900

    27th commit

--- webhook.php
+++ webhook.php
@@ -14,7 +14,7 @@ $hmac       = hash_hmac('sha1', $post_data, $SECRET_KEY);
 if (isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === 'sha1='.$hmac) {
   $payload = json_decode($post_data, true);
   if ($payload['ref'] == 'refs/heads/master') {
-    exec('cd /var/www/html && git pull', $output, $return_var);
+    exec('cd /home/nasu-medix/dev/php72a && git pull', $output, $return_var);
     file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$payload['ref']." git pulled: ".$payload['after']." ".$payload['commits'][0]['message']. " / " . $return_var . " : " .count($output) . "\n", FILE_APPEND|LOCK_EX);
   }
 }
