398cc6cf67bdcbc64ce2896dfad05ae65206c210
Takayasu Nasu <nasu@medix-inc.co.jp>
2018-02-06 15:13:43 +0900

    modified webhook

--- webhook.php
+++ webhook.php
@@ -5,7 +5,6 @@ if($_SERVER["REQUEST_METHOD"] != "POST") {
   exit;
 }
 
-$LOG_FILE   = dirname(__FILE__).'/hook.log';
 $SECRET_KEY = 'test.iprimo';
 $header     = getallheaders();
 $post_data  = file_get_contents('php://input');
@@ -14,7 +13,6 @@ $hmac       = hash_hmac('sha1', $post_data, $SECRET_KEY);
 if (isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === 'sha1='.$hmac) {
   $payload = json_decode($post_data, true);
   if ($payload['ref'] == 'refs/heads/master') {
-    exec('git pull 2>&1', $output, $return_var);
-    file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$payload['ref']." git pulled: ".$payload['after']." ".$payload['commits'][0]['message']. " / " . $return_var . " : " .$output[0] . $output[1] . $output[2] . "\n", FILE_APPEND|LOCK_EX);
+    exec('cd && git pull 2>&1', $output, $return_var);
   }
 }
